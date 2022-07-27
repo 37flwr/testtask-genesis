19bb8da6e90c57b2dee293ca6964106119b09831
37flwr <nesterishinyarik@gmail.com>
2022-07-27 23:45:43 +0300

    Add price getter api call; add nodemailer to dependencies

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 // import express from 'express';
 const express = require('express')
+const axios = require('axios')
+const nodemailer = require('nodemailer')
+// import axios from 'axios';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
+app.use(express.json())
+
+app.get('/price', (req, res) => {
+    axios.get('https://blockchain.info/ticker')
+        .then((response) => {
+            res.send(response.data.USD)
+        })
+})
+
+app.get('/subscribe/:email', (req, res) => {
+    res.send(req.params.email)
 })
 
 app.listen(3000, () => {
