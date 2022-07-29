bda30f754c98d4c7479e7de8a255b14178cfc886
37flwr <nesterishinyarik@gmail.com>
2022-07-29 16:13:00 +0300

    Add router; move logic to router

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const axios = require('axios')
 const Yup = require('yup')
 const nodemailer = require('nodemailer')
+const fs = require('fs')
+const path = require('path')
 
 const app = express();
 const router = express.Router();
-
-const formData = []
 
 router
 .route('/rate')
@@ -22,48 +22,53 @@ router
 router
 .route('/subscribe')
 .post(async (req, res) => {
-    if(req.body.email) {
-        const schema = Yup.object().shape({
-            email: Yup.string().email(),
-        });
-        const checkForValidity = await schema.isValid({email: req.body.email})
-        const alreadyRegistered = formData.includes(req.body.email)
-        if(checkForValidity && !alreadyRegistered) {
-            formData.push(req.body.email)
-            res.status(200).send(`E-mail додано`)
-        } else if(alreadyRegistered) {
-            res.status(409).send(`Юзер ${req.body.email} вже є у базі даних`)
+    if(!fs.readFileSync('users.txt', 'utf-8').split('\n').slice(0, -1).includes(req.body.email)) {
+        if(req.body?.email) {
+            const schema = Yup.object().shape({
+                email: Yup.string().email(),
+            });
+            const checkForValidity = await schema.isValid({email: req.body.email})
+            if(checkForValidity) {
+                fs.appendFileSync('users.txt', `${req.body.email}\n`, 'utf-8')
+                res.status(200).send(`E-mail додано`)
+            } else {
+                res.status(400).send('Е-mail не валідний. Спробуйте ввести e-mail за типом youremail@mail.com')
+            }
         } else {
-            res.status(400).send('Е-mail не валідний. Спробуйте ввести e-mail за типом youremail@mail.com')
-        }
+            res.status(400).send('Missing or insufficient parameters')
+        } 
     } else {
-        res.send('Missing or insufficient parameters')
+        res.status(409).send(`Юзер ${req.body.email} вже є у базі даних`)
     }
 })
 
 router
 .route('/subscribe:email')
 .post(async (req, res) => {
-    const schema = Yup.object().shape({
-        email: Yup.string().email(),
-    });
-    const checkForValidity = await schema.isValid({email: req.params.email})
-    const alreadyRegistered = formData.includes(req.params.email)
-
-    if(checkForValidity && !alreadyRegistered) {
-        formData.push(req.params.email)
-        res.send(`E-mail додано`)
-    } else if(alreadyRegistered) {
-        res.status(409).send(`Юзер ${req.params.email} вже є у базі даних`)
+    if(!fs.readFileSync('users.txt', 'utf-8').split('\n').slice(0, -1).includes(req.params.email)) {
+        if(req.params.email) {
+            const schema = Yup.object().shape({
+                email: Yup.string().email(),
+            });
+            const checkForValidity = await schema.isValid({email: req.params.email})
+            if(checkForValidity) {
+                fs.appendFileSync('users.txt', `${req.params.email}\n`, 'utf-8')
+                res.status(200).send(`E-mail додано`)
+            } else {
+                res.status(400).send('Е-mail не валідний. Спробуйте ввести e-mail за типом youremail@mail.com')
+            }
+        } else {
+            res.status(400).send('Missing or insufficient parameters')
+        } 
     } else {
-        res.status(400).send('Е-mail не валідний. Спробуйте ввести e-mail за типом youremail@mail.com')
+        res.status(409).send(`Юзер ${req.params.email} вже є у базі даних`)
     }
 })
 
 router
 .route('/sendEmails')
 .post(async (req, res) => {
-    if(formData.length > 0) {
+    if(fs.readFileSync('users.txt', 'utf-8').split('\n').slice(0, -1).length > 0) {
         const currentPrice = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=uah')
         
         var transporter = nodemailer.createTransport({
@@ -76,7 +81,7 @@ router
 
         let info = await transporter.sendMail({
             from: '"BTC Price Scanner" <37yaroslav@gmail.com', // sender address
-            to: formData.join(', '), // list of receivers
+            to: fs.readFileSync('users.txt', 'utf-8').split('\n').slice(0, -1).join(', '), // list of receivers
             subject: "BTC Price Rate ✔", // Subject line
             text: `Currenct BTC price: ${currentPrice.data.bitcoin.uah} UAH`, // plain text body
             html: `<span>Currenct BTC price: ${currentPrice.data.bitcoin.uah} UAH</span>`, // html body
@@ -92,82 +97,38 @@ router
     } else {
         res.status(400).send('Нажаль немає підписаних юзерів 😔')
     }
+    // if(formData.length > 0) {
+    //     const currentPrice = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=uah')
+        
+    //     var transporter = nodemailer.createTransport({
+    //         service: 'gmail',
+    //         auth: {
+    //             user: '37yaroslav@gmail.com',
+    //             pass: 'xaczvrexkyuvleia'
+    //         }
+    //     });
+
+    //     let info = await transporter.sendMail({
+    //         from: '"BTC Price Scanner" <37yaroslav@gmail.com', // sender address
+    //         to: formData.join(', '), // list of receivers
+    //         subject: "BTC Price Rate ✔", // Subject line
+    //         text: `Currenct BTC price: ${currentPrice.data.bitcoin.uah} UAH`, // plain text body
+    //         html: `<span>Currenct BTC price: ${currentPrice.data.bitcoin.uah} UAH</span>`, // html body
+    //     });
+
+    //     transporter.sendMail(info, function(error){
+    //         if (error) {
+    //             res.status(400).send(error)
+    //         } else {
+    //             res.status(200).send('E-mailʼи відправлено')
+    //         }
+    //     });
+    // } else {
+    //     res.status(400).send('Нажаль немає підписаних юзерів 😔')
+    // }
 })
 
-// app.get('/rate', (req, res) => {
-//     axios.get('https://blockchain.info/ticker')
-//         .then((response) => {
-//             res.send(response.data.USD)
-//         }).catch((error) => {
-//             res.send('Упс... Щось пішло не так' + error)
-//         })
-// })
-
-// app.post('/subscribe', async (req, res) => {
-//     const schema = Yup.object().shape({
-//         email: Yup.string().email(),
-//     });
-//     const checkForValidity = await schema.isValid({email: req.body.email})
-//     const alreadyRegistered = formData.includes(req.body.email)
-//     if(checkForValidity && !alreadyRegistered) {
-//         formData.push(req.body.email)
-//         res.send(`E-mail додано`)
-//     } else if(alreadyRegistered) {
-//         res.status(409).send(`Юзер ${req.body.email} вже є у базі даних`)
-//     } else {
-//         res.status(400).send('Е-mail не валідний. Спробуйте ввести e-mail за типом youremail@mail.com')
-//     }
-// })
-
-// app.post('/subscribe/:email', async (req, res) => {
-//     const schema = Yup.object().shape({
-//         email: Yup.string().email(),
-//     });
-//     const checkForValidity = await schema.isValid({email: req.params.email})
-//     const alreadyRegistered = formData.includes(req.params.email)
-
-//     if(checkForValidity && !alreadyRegistered) {
-//         formData.push(req.params.email)
-//         res.send(`E-mail додано`)
-//     } else if(alreadyRegistered) {
-//         res.status(409).send(`Юзер ${req.params.email} вже є у базі даних`)
-//     } else {
-//         res.status(400).send('Е-mail не валідний. Спробуйте ввести e-mail за типом youremail@mail.com')
-//     }
-// })
-
-// app.post('/sendEmails', async (req, res) => {
-//     if(formData.length > 0) {
-//         const currentPrice = await axios.get('https://blockchain.info/ticker')
-        
-//         var transporter = nodemailer.createTransport({
-//             service: 'gmail',
-//             auth: {
-//                 user: '37yaroslav@gmail.com',
-//                 pass: 'xaczvrexkyuvleia'
-//             }
-//         });
-
-//         let info = await transporter.sendMail({
-//             from: '"BTC Price Scanner" <37yaroslav@gmail.com', // sender address
-//             to: formData.join(', '), // list of receivers
-//             subject: "BTC Price Rate ✔", // Subject line
-//             text: `Currenct BTC price: $ ${currentPrice.data.USD.last}`, // plain text body
-//             html: `<span>Currenct BTC price: $ ${currentPrice.data.USD.last}</span>`, // html body
-//         });
-
-//         transporter.sendMail(info, function(error){
-//             if (error) {
-//                 res.status(400).send(error)
-//             } else {
-//                 res.status(200).send('E-mailʼи відправлено')
-//             }
-//         });
-//     } else {
-//         res.status(400).send('Нажаль немає підписаних юзерів 😔')
-//     }
-// })
-
+app.use(express.json());
 app.use('/api', router)
 
 app.listen(3000, () => {
